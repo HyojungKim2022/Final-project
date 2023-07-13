f916eaef415f2972faf88dd97878581bd8dc9064
Kim YeongGwang <92590654+Glory-K@users.noreply.github.com>
2023-07-13 09:26:16 +0900

    Delete db_test.py

--- BLC/views/admin_views/db_test.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from BLC.models import Items, Store, Stock
-
-def post_view(request):
-    items = Items.objects.all()
-    stores = Store.objects.all()
-    stocks = Stock.objects.all()
-    return render(request, 'BLC/test.html', {'items' : items, 'stores':stores, 'stocks':stocks})
-
